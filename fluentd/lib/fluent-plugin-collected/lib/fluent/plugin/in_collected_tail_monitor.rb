ViaQ/logging-fluentd

commit dac3eff6c37237ae91ddf95915f6d163445c4d70

LOG-1722: Update fluentd to 1.14.5

fluentd/lib/fluent-plugin-collected/lib/fluent/plugin/in_collected_tail_monitor.rb
@@ -95,11 +95,11 @@ module Fluent::Plugin
           if pe.read_inode == old_inode && pe.read_pos >= old_pos
             # Same file, has not been truncated since last we looked. Add the delta.
             @log.trace "delta bytes #{pe.read_pos}  #{old_pos}"
-            @metrics[:total_bytes_collected].increment(label, pe.read_pos - old_pos)
+            @metrics[:total_bytes_collected].increment(by: pe.read_pos - old_pos, labels: label)
           else
             # Changed file or truncated the existing file. Add the initial content.
             @log.trace "delta bytes #{pe.read_pos}"
-            @metrics[:total_bytes_collected].increment(label, pe.read_pos)
+            @metrics[:total_bytes_collected].increment(by: pe.read_pos, labels: label)
           end
         end
       end
@@ -141,7 +141,7 @@ module Fluent::Plugin
       if @registry.exist?(name)
         @registry.get(name)
       else
-        @registry.counter(name, docstring)
+        @registry.counter(name, docstring: docstring, labels: [:hostname, :plugin_id, :type, :path, :namespace, :podname, :containername])
       end
     end
 

commit 86d802523c1ab9ed76ebeb616d004eae1c798130

LOG-2220: Fluentd collector not setting labels from /var/log/pods paths
Metric labels were incorrectly set as a "labels" field in the metric,
rather than at the top level.
Regression introduced in:  dac3eff6c | LOG-1722: Update fluentd to 1.14.5

Undetected due to lack of tests to verify the published metric -
unit test only validate the label set internally, it was being
misapplied to the metric.

fluentd/lib/fluent-plugin-collected/lib/fluent/plugin/in_collected_tail_monitor.rb
@@ -29,8 +29,6 @@ module Fluent::Plugin
     def initialize
       super
       @registry = ::Prometheus::Client.registry
-      # As per k8 regex for container logfile symlink ref :
-      # https://github.com/fabric8io/fluent-plugin-kubernetes_metadata_filter/blob/master/lib/fluent/plugin/filter_kubernetes_metadata.rb#L56
     end
 
     def multi_workers_ready?
@@ -47,6 +45,7 @@ module Fluent::Plugin
         unless value.is_a?(String)
           raise Fluent::ConfigError, "record accessor syntax is not available in collected_tail_monitor"
         end
+
         @base_labels[key] = expander.expand(value)
       end
 
@@ -69,15 +68,14 @@ module Fluent::Plugin
       timer_execute(:in_collected_tail_monitor, @interval, &method(:update_monitor_info))
     end
 
-
     def update_monitor_info
       opts = {
         ivars: MONITOR_IVARS,
       }
 
-     agent_info = @monitor_agent.plugins_info_all(opts).select do |info|
+      agent_info = @monitor_agent.plugins_info_all(opts).select do |info|
         info['type'] == 'tail'.freeze
-     end
+      end
       agent_info.each do |info|
         tails = info['instance_variables'][:tails]
         next if tails.nil?
@@ -96,11 +94,11 @@ module Fluent::Plugin
           if pe.read_inode == old_inode && pe.read_pos >= old_pos
             # Same file, has not been truncated since last we looked. Add the delta.
             @log.trace "delta bytes #{pe.read_pos}  #{old_pos}"
-            @metrics[:total_bytes_collected].increment(by: pe.read_pos - old_pos, labels: label)
+            @metrics[:total_bytes_collected].increment(label, pe.read_pos)
           else
             # Changed file or truncated the existing file. Add the initial content.
             @log.trace "delta bytes #{pe.read_pos}"
-            @metrics[:total_bytes_collected].increment(by: pe.read_pos, labels: label)
+            @metrics[:total_bytes_collected].increment(label, pe.read_pos)
           end
         end
       end

fluentd/lib/fluent-plugin-collected/test/plugin/test_in_collected_tail_monitor.rb
@@ -1,5 +1,5 @@
-require "test_helper"
-require "fluent/plugin/in_collected_tail_monitor"
+require 'test_helper'
+require 'fluent/plugin/in_collected_tail_monitor'
 
 class CollectedTailMonitorInputTest < Test::Unit::TestCase
   include Fluent
@@ -8,16 +8,12 @@ class CollectedTailMonitorInputTest < Test::Unit::TestCase
     Fluent::Test.setup
   end
 
-  MONITOR_CONFIG = %[
-  @type tail
-     path /tmp/tmp.log, /var/log/containers/mypodname_mynamespace_mycontainername-34646d7fb38199129ab8d0e6f41833d26e1826cba92571100fd6c53904a5317e.log
-  
-  @type collected_tail_monitor
+  MONITOR_CONFIG = %(
+    @type collected_tail_monitor
     <labels>
-      tag mytag
-      host example.com
+      foo bar
     </labels>
-]
+    )
 
   INVALID_MONITOR_CONFIG = %[
   @type collected_tail_monitor
@@ -34,48 +30,34 @@ class CollectedTailMonitorInputTest < Test::Unit::TestCase
     Fluent::Test::Driver::Input.new(Fluent::Plugin::CollectedTailMonitorInput).configure(conf)
   end
 
-  def test_configure
+  def test_labels_applied_to_metrics
     d = create_driver(MONITOR_CONFIG)
+    d.run do
+      d.instance.update_monitor_info
+
+      # Test /var/log/containers format
+    path = '/var/log/containers/mypodname_mynamespace_mycontainername-34646d7fb38199129ab8d0e6f41833d26e1826cba92571100fd6c53904a5317e.log'
+      labels = d.instance.labels({ 'plugin_id' => 'mypluginid', 'type' => 'input_plugin' }, path)
+      assert_equal('mynamespace', labels[:namespace])
+      assert_equal('mycontainername', labels[:containername])
+      assert_equal('mypodname', labels[:podname])
+      assert_equal('mypluginid', labels[:plugin_id])
+      assert_equal('bar', labels[:foo])
+
+      # Test /var/log/pods format
+      path = '/var/log/pods/mynamespace2_mypodname2_05682f61-8a44-47af-ae0f-41ad3792e20a/mycontainername2/1.log'
+      labels = d.instance.labels({ 'plugin_id' => 'mypluginid', 'type' => 'input_plugin' }, path)
+      assert_equal('mynamespace2', labels[:namespace])
+      assert_equal('mycontainername2', labels[:containername])
+      assert_equal('mypodname2', labels[:podname])
+      assert_equal('bar', labels[:foo])
+    end
   end
 
-  def test_labels_applied_to_metrics
-    conf = MONITOR_CONFIG
-    puts "passing this #{conf}"
-    d = create_driver(conf)
-    beforerunlabels = d.instance.instance_variable_get(:@base_labels)
-    puts "before base labels set to ...#{beforerunlabels}"
-    d.run {
-    d.instance.update_monitor_info()
-    postrunlabels = d.instance.instance_variable_get(:@base_labels)
-    path = "/tmp/tmp.log"
-    mergedlabels  = d.instance.labels({"plugin_id" => "mypluginid", "type" => "input_plugin"}, path)
-    puts "with logfilepath as #{path} post merging base labels set to ...#{mergedlabels}"
-
-    # Test /var/log/containers format
-    path = "/var/log/containers/mypodname_mynamespace_mycontainername-34646d7fb38199129ab8d0e6f41833d26e1826cba92571100fd6c53904a5317e.log"
-    newmergedlabels  = d.instance.labels({"plugin_id" => "mypluginid", "type" => "input_plugin"}, path)
-    puts "with logfilepath as #{path} post merging base labels set to ...#{newmergedlabels}"
-
-    assert_equal('mynamespace',newmergedlabels[:namespace])
-    assert_equal('mycontainername',newmergedlabels[:containername])
-    assert_equal('mypodname',newmergedlabels[:podname])
-
-
-    # Test /var/log/pods format
-    path = "/var/log/pods/mynamespace2_mypodname2_05682f61-8a44-47af-ae0f-41ad3792e20a/mycontainername2/1.log"
-    newmergedlabels  = d.instance.labels({"plugin_id" => "mypluginid", "type" => "input_plugin"}, path)
-    puts "with logfilepath as #{path} post merging base labels set to ...#{newmergedlabels}"
-    assert_equal('mypluginid',newmergedlabels[:plugin_id])
-    assert_equal('mynamespace2',newmergedlabels[:namespace])
-    assert_equal('mycontainername2',newmergedlabels[:containername])
-    assert_equal('mypodname2',newmergedlabels[:podname])
-    }
- end
-
   def test_invalid_configure
-      assert_raise(Fluent::ConfigError) {
-        d = create_driver(INVALID_MONITOR_CONFIG)
-      }
+    assert_raise(Fluent::ConfigError) do
+      create_driver(INVALID_MONITOR_CONFIG)
+    end
   end
 
   test 'emit' do
@@ -84,9 +66,8 @@ class CollectedTailMonitorInputTest < Test::Unit::TestCase
 
     d.events.each do |tag, time, record|
       assert_equal('input.test', tag)
-      assert_equal({'plugin_id' => 'fluentd','type' => 'tail'}, record)
+      assert_equal({ 'plugin_id' => 'fluentd', 'type' => 'tail' }, record)
       assert(time.is_a?(Fluent::EventTime))
     end
   end
-
 end