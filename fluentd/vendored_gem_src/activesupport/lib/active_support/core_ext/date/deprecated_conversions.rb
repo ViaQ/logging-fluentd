003552298ffaab8b0caedf599580bb1dab077207
Jeff Cantrill <jcantril@redhat.com>
2023-12-14 16:31:30 -0500

    LOG-4489: address CVE-2023-38037

--- fluentd/vendored_gem_src/activesupport/lib/active_support/core_ext/date/deprecated_conversions.rb
+++ fluentd/vendored_gem_src/activesupport/lib/active_support/core_ext/date/deprecated_conversions.rb
@@ -15,7 +15,21 @@ class Date
         strftime(formatter)
       end
     elsif format == NOT_SET
-      to_default_s
+      if formatter = DATE_FORMATS[:default]
+        ActiveSupport::Deprecation.warn(<<-MSG.squish)
+          Using a :default format for Date#to_s is deprecated. Please use Date#to_fs instead. If you fixed all places
+          inside your application that you see this deprecation, you can set
+          `ENV['RAILS_DISABLE_DEPRECATED_TO_S_CONVERSION']` to `"true"` in the `config/application.rb` file before
+          the `Bundler.require` call to fix all the callers outside of your application.
+        MSG
+        if formatter.respond_to?(:call)
+          formatter.call(self).to_s
+        else
+          strftime(formatter)
+        end
+      else
+        to_default_s
+      end
     else
       ActiveSupport::Deprecation.warn(
         "Date#to_s(#{format.inspect}) is deprecated. Please use Date#to_fs(#{format.inspect}) instead."
--- fluentd/vendored_gem_src/activesupport/lib/active_support/core_ext/range/deprecated_conversions.rb
+++ fluentd/vendored_gem_src/activesupport/lib/active_support/core_ext/range/deprecated_conversions.rb
@@ -10,7 +10,17 @@ module ActiveSupport
         )
         formatter.call(first, last)
       elsif format == NOT_SET
-        super()
+        if formatter = RangeWithFormat::RANGE_FORMATS[:default]
+          ActiveSupport::Deprecation.warn(<<-MSG.squish)
+            Using a :default format for Range#to_s is deprecated. Please use Range#to_fs instead. If you fixed all
+            places inside your application that you see this deprecation, you can set
+            `ENV['RAILS_DISABLE_DEPRECATED_TO_S_CONVERSION']` to `"true"` in the `config/application.rb` file before
+            the `Bundler.require` call to fix all the callers outside of your application.
+          MSG
+          formatter.call(first, last)
+        else
+          super()
+        end
       else
         ActiveSupport::Deprecation.warn(
           "Range#to_s(#{format.inspect}) is deprecated. Please use Range#to_fs(#{format.inspect}) instead."
